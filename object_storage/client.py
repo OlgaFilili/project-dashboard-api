9d99b990b88195da6a408aabd957a6a282281139
OlgaFilili <philippova.ov@gmail.com>
2026-07-02 15:58:48 +0300

    feat: add MinIO object storage infrastructure

---
+++ object_storage/client.py
@@ -0,0 +1,23 @@
+import boto3
+from botocore.exceptions import ClientError
+
+from config.config import MINIO_ENDPOINT, MINIO_ROOT_USER, MINIO_ROOT_PASSWORD, MINIO_BUCKET
+
+storage_client = boto3.client(
+    service_name="s3",
+    endpoint_url=f"http://{MINIO_ENDPOINT}",
+    aws_access_key_id=MINIO_ROOT_USER,
+    aws_secret_access_key=MINIO_ROOT_PASSWORD)
+
+
+async def init_storage():
+    try:
+        storage_client.head_bucket(Bucket=MINIO_BUCKET)
+
+    except ClientError as exc:
+        error_code = exc.response["Error"]["Code"]
+
+        if error_code in ("404", "NoSuchBucket"):
+            storage_client.create_bucket(Bucket=MINIO_BUCKET)
+        else:
+            raise
